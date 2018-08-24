b5de09e15be04318e45620e30ef62b972362e985
Andrew <andrewhuh67@gmail.com>
2018-08-24 09:45:33 -0700

    Setting up Spring Application

---
+++ WebContent/WEB-INF/BillSplitter.jsp
@@ -0,0 +1,53 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+
+</head>
+<body>
+
+	<h1>Bill-Splitter</h1>
+	
+	<form action="/Bill-Splitter/submitAdmissionForm.html" method="post">
+		<p>
+			Person 1 Amount: <input type="text" name="studentName" />
+			
+		</p>
+		
+		<p>
+			Person 2 Amount: <input type="text" name="studentHobby" />
+		</p>
+		
+		<p>
+			Person 3 Amount: <input type="text" name="studentHobby" />
+		</p>
+		
+		<p>
+			Person 4 Amount: <input type="text" name="studentHobby" />
+		</p>
+		
+		<p>
+			Tax: <input type="text" name="studentHobby" />
+		</p>
+		
+		<p>
+			Delivery Fee: <input type="text" name="studentHobby" />
+		</p>
+		
+		<p>
+			Tip: <input type="text" name="studentHobby" />
+		</p>
+		
+	
+	</form>
+
+
+
+
+
+	
+	<h2>${msg}</h2>
+</body>
+</html>
---
+++ src/com/andrewhuh/billcontroller/BillSplitterController.java
@@ -0,0 +1,35 @@
+package com.andrewhuh.billcontroller;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BillSplitterController {
+	
+	@RequestMapping(value="/billsplitter.html", method = RequestMethod.GET)  
+	public ModelAndView getBillSplitterForm() {
+		
+		ModelAndView model = new ModelAndView("BillSplitter");
+		
+		return model;
+	}
+	@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
+	public ModelAndView submitAdmissionForm(
+			@RequestParam("studentName") String name, @RequestParam("studentHobby") String hobby){
+		
+		ModelAndView model = new ModelAndView("AdmissionSuccess");
+		model.addObject("msg", "Details subitted by you:: Name: " + name + ", Hobby: " + hobby);
+		
+		return model;
+	}
+	
+}
+
